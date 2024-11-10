149f4eba3d65145a6b634e8037c6bed62c7319c2
rohithaider <rohithaider@gmail.com>
2024-11-11 03:06:27 +0600

    deploy test 4 resolved 2

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,17 +1,20 @@
+/* eslint-disable react/prop-types */
 import { NavLink } from "react-router-dom";
 
-export default function Categories({ categories = [] }) {
+export default function Categories({ categories }) {
+  
   return (
+  
     <div
       role="tablist"
       className="tabs tabs-boxed flex flex-col space-y-2 bg-white border items-center p-6 "
     >
-      {Array.isArray(categories) && categories.map((category) => (
+      {categories.map((category) => (
         <NavLink
           key={category.category}
           to={`/category/${category.category}`}
           role="tab"
-          className={({ isActive }) => `btn w-full rounded-2xl ${isActive ? 'tab-active' : ''}`}
+          className={({isActive})=>`btn w-full rounded-2xl ${isActive?'tab-active':''}`}
         >
           {category.category}
         </NavLink>
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,17 +1,16 @@
-import { Link,  useLocation,useLoaderData, Outlet } from "react-router-dom";
+import { Link, useLocation, useLoaderData, Outlet } from "react-router-dom";
 
 import Heroimage from "../components/Heroimage";
 import Categories from "../components/Categories";
 export default function Homepage() {
-    const categories = useLoaderData()
-    
-    
-    const location = useLocation();
-    const isHomePage = location.pathname ==="/"
-    
+  const categories = useLoaderData();
+
+  const location = useLocation();
+  const isHomePage = location.pathname === "/";
+
   return (
     <>
-        <div className="hero bg-[#9538E2] min-h-96 rounded-b-xl relative">
+      <div className="hero bg-[#9538E2] min-h-96 rounded-b-xl relative">
         <div className="hero-content text-center text-white">
           <div className="max-w-3xl">
             <h1 className="text-5xl font-bold ">
@@ -22,25 +21,31 @@ export default function Homepage() {
               next level. From smart devices to the coolest accessories, we have
               it all!
             </p>
-            
-            {isHomePage && <Link to="dashboard"><button className="btn rounded-2xl bg-white text-[#9538E2] mb-10">Shop Now</button></Link>}
+
+            {isHomePage && (
+              <Link to="dashboard">
+                <button className="btn rounded-2xl bg-white text-[#9538E2] mb-10">
+                  Shop Now
+                </button>
+              </Link>
+            )}
           </div>
         </div>
-    
       </div>
-      <Heroimage/>
+      <Heroimage />
       <div>
-        <h1 className="text-center font-bold text-4xl mb-6">Explore Cutting-Edge Gadgets</h1>
+        <h1 className="text-center font-bold text-4xl mb-6">
+          Explore Cutting-Edge Gadgets
+        </h1>
       </div>
-        <div className="flex gap-3">
+      <div className="flex gap-3">
         <div className="w-2/12">
-            <Categories categories={categories}/>
+          <Categories categories={categories} />
         </div>
         <div className="w-10/12">
-        <Outlet/>
+          <Outlet />
         </div>
-
-        </div>
-      </>
+      </div>
+    </>
   );
 }
