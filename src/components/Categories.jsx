e7dc48d7ae58d059ff5cacff74e20311bed9b378
rohithaider <rohithaider@gmail.com>
2024-11-11 02:54:17 +0600

    deploy test 2

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -3,20 +3,21 @@ import { NavLink } from "react-router-dom";
 
 export default function Categories({ categories }) {
   return (
-    <div
-      role="tablist"
-      className="tabs tabs-boxed flex flex-col space-y-2 bg-white border items-center p-6 "
-    >
-      {categories.map((category) => (
-        <NavLink
-          key={category.category}
-          to={`/category/${category.category}`}
-          role="tab"
-          className={({isActive})=>`btn w-full rounded-2xl ${isActive?'tab-active':''}`}
-        >
-          {category.category}
-        </NavLink>
-      ))}
-    </div>
+    <h1>Test</h1>
+    // <div
+    //   role="tablist"
+    //   className="tabs tabs-boxed flex flex-col space-y-2 bg-white border items-center p-6 "
+    // >
+    //   {categories.map((category) => (
+    //     <NavLink
+    //       key={category.category}
+    //       to={`/category/${category.category}`}
+    //       role="tab"
+    //       className={({isActive})=>`btn w-full rounded-2xl ${isActive?'tab-active':''}`}
+    //     >
+    //       {category.category}
+    //     </NavLink>
+    //   ))}
+    // </div>
   );
 }
