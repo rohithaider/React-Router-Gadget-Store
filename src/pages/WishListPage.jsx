83147ec08b7304685abb9d338542c61bb498742c
rohithaider <rohithaider@gmail.com>
2024-11-11 16:30:20 +0600

    Added wishlist helmet

--- src/pages/WishListPage.jsx
+++ src/pages/WishListPage.jsx
@@ -1,14 +1,21 @@
-import CartCard from "../components/CartCard";
+import { Helmet } from "react-helmet-async";
+
 import WishListCard from "../components/WishListCard";
 import { useWishlist } from "../context/WishListContext";
 
 export default function WishListPage(){
     const{wishlistItems} = useWishlist()
     return (
-        <div>
+       <div>
+         <Helmet>
+        <title>Wishlist</title>
+        <link rel="icon" type="image/png" href="/public/assets/favicon-16x16.png" />
+      </Helmet>
+         <div>
         {wishlistItems.map((item) => (
           <WishListCard key={crypto.randomUUID()} item={item} />
         ))}
       </div>
+       </div>
     );
 }
