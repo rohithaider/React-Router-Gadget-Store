f6d532f7b8026f4b4f44d7819ef77e06f4aa8d3a
rohithaider <rohithaider@gmail.com>
2024-11-11 14:22:40 +0600

    Dasboard cart's header implemented

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,5 +1,21 @@
-export default function CartPage(){
-    return (
-        <h2>Cart Page</h2>
-    );
+import { FaSortAmountDown } from "react-icons/fa";
+export default function CartPage() {
+  return (
+    <div className="mt-2">
+      <div className="flex justify-between items-center">
+        <div>
+          <h1 className="font-bold">Cart</h1>
+        </div>
+        <div className="flex items-center gap-2">
+          <h1 className="font-bold">Total Cost: $99</h1>
+
+          <div className="btn btn-outline flex items-center gap-2 border border-purple-600 rounded-3xl text-purple-600 p-2 ">
+            <button>Sort by Price</button>
+            <FaSortAmountDown />
+          </div>
+          <button className="bg-purple-600 text-white border rounded-3xl p-2 btn btn-outline ">Purchase</button>
+        </div>
+      </div>
+    </div>
+  );
 }
